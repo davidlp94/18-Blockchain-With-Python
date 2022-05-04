d65561fdc4ea198cf9c8c2eeb931ff65d04dff93
davidlp94 <96163075+davidlp94@users.noreply.github.com>
2022-05-03 21:16:36 -0400

    Updating code and added new dataclass.

--- pychain.py
+++ pychain.py
@@ -46,10 +46,13 @@ import hashlib
 # Note that you’ll use this new `Record` class as the data type of your `record` attribute in the next section.
 
 
-# @TODO
 # Create a Record Data Class that consists of the `sender`, `receiver`, and
 # `amount` attributes
-# YOUR CODE HERE
+@dataclass
+class Record:
+    sender = str
+    receiver = str
+    amount = float
 
 
 ################################################################################
@@ -68,7 +71,7 @@ class Block:
 
     # @TODO
     # Rename the `data` attribute to `record`, and set the data type to `Record`
-    data: Any
+    record = Record
 
     creator_id: int
     prev_hash: str = "0"
@@ -166,19 +169,18 @@ pychain = setup()
 
 # @TODO:
 # Delete the `input_data` variable from the Streamlit interface.
-input_data = st.text_input("Block Data")
 
 # @TODO:
 # Add an input area where you can get a value for `sender` from the user.
-# YOUR CODE HERE
+input_value_sender = st.text_input("Enter Sender Information")
 
 # @TODO:
 # Add an input area where you can get a value for `receiver` from the user.
-# YOUR CODE HERE
+input_value_receiver = st.text_input("Enter Receiver Information")
 
 # @TODO:
 # Add an input area where you can get a value for `amount` from the user.
-# YOUR CODE HERE
+input_value_amount = st.text_input("Enter Amount to Send")
 
 if st.button("Add Block"):
     prev_block = pychain.chain[-1]
